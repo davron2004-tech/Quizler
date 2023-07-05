517183205e4dbb2ee1fe7fabfcb5628c884efe40
Davron Abdukhakimov <davron0506150@gmail.com>
2023-07-05 15:39:22 +0500

    Initial Commit

---
+++ Quizler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizler
+//
+//  Created by Davron Abdukhakimov on 05/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
